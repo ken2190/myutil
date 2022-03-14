bd752ca832286e178b844f235f091990ddcafa22
Kev. Noel <arita37@users.noreply.github.com>
2022-03-14 15:56:05 +0900

    ok

--- utilmy/utilmy.py
+++ utilmy/utilmy.py
@@ -246,21 +246,23 @@ def import_function(fun_name=None, module_name=None, fuzzy_match=False):
         raise Exception( msg )  
 
 
-def glob_glob(dirin:Union[str, list]="**/*.py", nfile=1000, direxclude:Union[str, list]="", recursive=True, silent=False, show=0, **kw):
-    """  
+def glob_glob(dirin:Union[str, list]="**/*.py", nfile=1000, direxclude:Union[str, list]="",  exclude:Union[str, list]="",  recursive=True, silent=False, show=0, **kw):
+    """  List of files.
        dirin:      **/*.py   any sub-directories or list of sub-directories
        direxclude: **/*.py   any sub-directories or list of sub-directories
 
     """
     import glob
-    ## Inside
+    ### Inside
     dirin = [dirin] if isinstance(dirin, str) else dirin
     flist = []
     for fi in dirin :
        flist  =  flist + glob.glob(fi , recursive= recursive, **kw )
     flist = list(set(flist))
 
-    ## Outside
+
+    ### Outside
+    direxclude = exclude if len(exclude) > 3 else direxclude  ### Alias only
     direxclude = [direxclude] if isinstance(dirin, str) else direxclude
     exclud = []
     for fi in direxclude :
@@ -269,6 +271,16 @@ def glob_glob(dirin:Union[str, list]="**/*.py", nfile=1000, direxclude:Union[str
 
     flist  = [fi for fi in flist if  fi not in exclud ]
     flist  = sorted(flist)
+
+    #### Filter by modified time
+    import datetime 
+    def os_file_modified_time(filename):
+        t = os.path.getmtime(filename)
+        return  datetime.datetime.fromtimestamp(t, tz=datetime.timezone.utc)     
+    fdates = [ os_file_modified_time(fi)  for fi in flist ]
+
+
+    ####  Details
     flist  = flist[:nfile]
 
     if not slient : log('Nfile: ', len(flist), str(flist)[:100])
