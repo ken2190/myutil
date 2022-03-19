fd7c3e229dbc6557bea4221ab745f82c994e29bf
Kev. Noel <arita37@users.noreply.github.com>
2022-03-19 15:15:33 +0900

    ok3

--- utilmy/deeplearning/torch/sentences.py
+++ utilmy/deeplearning/torch/sentences.py
@@ -15,9 +15,7 @@ train Sentence Transformer with different Losses such as:**
 > TripletHard Loss
 > MultpleNegativesRanking Loss
 
-#!pip3 install python-box
 # !pip install sentence-transformers
-#!pip3 install tensorflow
 
 We create a new end-to-end example on how to use a custom inference.py script w
 ith a Sentence Transformer and a mean pooling layer to create sentence embeddings.🤯
