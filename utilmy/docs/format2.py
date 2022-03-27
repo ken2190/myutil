8c04f20472c9101bb4900cc5d93236597d8aba50
Kev. Noel <arita37@users.noreply.github.com>
2022-03-27 21:35:02 +0900

    ok

--- utilmy/docs/format2.py
+++ utilmy/docs/format2.py
@@ -1,16 +1,270 @@
 # -*- coding: utf-8 -*-
-MNAME = "utilmy.template"
-HELP = """ utils for """
+MNAME = "utilmy.docs.format2"
+HELP = """ utils for re-formatting files using TEMPLATES
 
 
-import re
-import os
-import glob
+"""
+
+import re, os, sys, glob
 from pprint import pprint
 
-
+#################################################################################################################
 # from utilmy import log, log2, help_create
 
+def log(*s) : 
+    print(*s, flush=True)
+
+
+#################################################################################################################
+def test1():
+    read_and_normalize_file2('test_script/test_script_no_header.py', 'test_script/output/test_script_no_header.py')
+    read_and_normalize_file2('test_script/test_script_no_logger.py', 'test_script/output/test_script_no_logger.py')
+    read_and_normalize_file2('test_script/test_script_no_core.py', 'test_script/output/test_script_no_core.py')
+    read_and_normalize_file2('test_script/test_script_normalize_import.py', 'test_script/output/test_script_normalize_import.py')
+
+
+    read_and_normalize_file3('test_script/test_script_normalize_import.py', 'test_script/output')
+    read_and_normalize_file3('test_script/test_script_no_header.py', 'test_script/output')
+    read_and_normalize_file3('test_script/test_script_no_logger.py', 'test_script/output')
+    read_and_normalize_file3('test_script/test_script_no_core.py', 'test_script/output')
+
+
+#################################################################################################################
+def read_and_normalize_file(file_path):
+    all_lines = get_file(file_path)
+    info = extrac_block(all_lines)
+
+    new_headers = normalize_header(file_path, info['header'])
+    new_imports = normalize_import(info['import'])
+    new_loggers = normalize_logger(info['logger'])
+    new_tests =   normalize_test(info['test'])
+    new_cores =   normalize_core(info['core'])
+    new_footers = normalize_footer(info['footer'])
+
+    # Create new data array then write to new file
+    new_all_lines = []
+    new_all_lines.extend(new_headers)
+    new_all_lines.extend(new_imports)
+    new_all_lines.extend(new_loggers)
+    new_all_lines.extend(new_tests)
+    new_all_lines.extend(new_cores)
+    new_all_lines.extend(new_footers)
+
+    # ss = "\n".join(new_all_lines)
+    return new_all_lines
+
+
+def read_and_normalize_file2(file_path, output_file):
+    new_all_lines = read_and_normalize_file(file_path)
+    # new_all_lines = ss.split("\n")
+    print(str(new_all_lines)[:100] )
+    with open(output_file, 'w+', encoding='utf-8') as f:
+        f.writelines(new_all_lines)
+
+
+def read_and_normalize_file3(file_path, output_file):
+    ## Safe Modification
+    batch_format_file(in_file= file_path, dirout= output_file, 
+                    format_list= [ read_and_normalize_file ])
+
+
+#################################################################################################################
+def normalize_header(file_name, lines):
+    """Nomarlize Header block
+
+    Args input is a array of lines.
+    """
+    #### not need of regex, code easier to read 
+
+    lines2 = []
+    if len(lines) >= 3:
+        # line 1
+        if '# -*- coding: utf-8 -*-' not  in lines[0] :
+            lines2.append('# -*- coding: utf-8 -*-\n')
+
+        # line 2
+        if 'MNAME' not in lines[1] :   ### MNAME = "utilmy.docs.format"
+            nmane =  ".".join( os.path.abspath(file_name).split("\\")[-4:] )[:-3]
+            lines2.append( f'MNAME = "{nmane}"\n')
+
+        if 'HELP' not in lines[2] :   ### HELP
+            lines2.append( f'HELP = "util"\n')
+
+    else:
+        lines2.append('# -*- coding: utf-8 -*-\n')
+        nmane =  ".".join( os.path.abspath(file_name).split("\\")[-4:] )[:-3]
+        lines2.append( f'MNAME = "{nmane}"\n')
+        lines2.append( f'HELP = "util"\n')
+
+    ### Add previous line
+    lines2.extend(lines)
+    # print(lines2)
+    return lines2
+
+
+def normalize_import(lines):
+    """  merge all import in one line and append others
+
+    """
+    import_list = []
+    from_list = []
+    lines2 = []
+    for line in lines :
+        if "import " in line :
+            if "from " in line : from_list.append(line)
+            else :               import_list.append(line)
+
+    ### Merge all import in one line   ################################################
+    llall = []
+    for imp in import_list :
+        imp = imp[6:] # remove import
+        ll    = [ t.strip() for t in  imp.split(",") if 'import' not in t ]
+        llall = llall + ll
+
+    lall = sorted( llall )
+
+    lines2 = []
+    ss = "import "
+    for mi in lall:
+        ss =  ss + mi + ", "
+        if len(ss) >  90 :
+            lines2.append( f"{ss[:-2]}\n" )  
+            ss = "import "
+    lines2.append(f"{ss[:-2]}\n")  
+
+    # lines2.extend(from_list)
+    # print(lines2)
+
+    #### Remaining import 
+    for ii, line in enumerate(lines):
+        if ii > 100 : break
+        if line.startswith("import ") : continue  ### Remove Old import
+        lines2.append(line)  
+
+    #### 
+    # print(lines2)
+    return lines2
+
+
+def normalize_logger(lines):
+    lines2 = []
+    if len(lines) >0:
+        if "from utilmy import log" not in lines[0]:
+            lines2.append('from utilmy import log, log2, help_create\n')
+    else:
+        lines2.append('from utilmy import log, log2, help_create\n')
+    # append all
+    lines2.extend(lines)
+
+    # check help function:
+    is_exist_help = False
+    for line in lines:
+        if line.startswith("def help"):
+            is_exist_help = True
+            break
+    
+    if not is_exist_help:
+        lines2.extend([
+            'def help():\n',
+            '   print( HELP + help_create(MNAME) )\n',
+            '\n',
+            '\n'
+        ])
+    # print(lines2)
+    return lines2
+
+
+def normalize_test(lines):
+    lines2 = []
+
+    # check test_all function:
+    is_exist_test_all = False
+    for line in lines:
+        if line.startswith("def test_all"):
+            is_exist_test_all = True
+            break
+    
+    if not is_exist_test_all:
+        lines2.extend([
+            'def test_all() -> None:\n',
+            '    """function test_all"""\n',
+            '    log(MNAME)\n',
+            '    test1()\n',
+            '    test2()\n',
+            '\n',
+            '\n',
+        ])
+
+    # check test1 function:
+    is_exist_test1 = False
+    for line in lines:
+        if line.startswith("def test1"):
+            is_exist_test1 = True
+            break
+    
+    if not is_exist_test1:
+        lines2.extend([
+            'def test1() -> None:\n',
+            '    """function test"""\n',
+            '    pass\n',
+            '\n',
+            '\n',
+        ])
+
+    # # check test2 function:
+    # is_exist_test2 = False
+    # for line in lines:
+    #     if line.startswith("def test2"):
+    #         is_exist_test2 = True
+    #         break
+    
+    # if not is_exist_test2:
+    #     lines2.extend([
+    #         'def test2() -> None:\n',
+    #         '    """function test"""\n',
+    #         '    pass\n',
+    #         '\n',
+    #         '\n',
+    #     ])
+
+    ###### Eztend lines
+    lines2.extend(lines)
+    return lines2
+
+
+def normalize_core(lines):
+    return lines
+
+
+def normalize_footer(lines):
+
+    lines2 = []
+
+    # check test_all function:
+    main_start_line = 0
+    is_exist_main = False
+    for ii, line in enumerate(lines):
+        if line.startswith("if __name__"):
+            is_exist_main = True
+            main_start_line = ii
+            break
+    if not is_exist_main:
+        lines2.extend(lines)
+        lines2.extend([
+            'if __name__ == "__main__":\n',
+            '   import fire\n',
+            '   fire.Fire()\n',
+        ])
+    else:
+        lines2.extend(lines)
+        if  not ('import fire' in lines[main_start_line+1]  and 'fire.Fire()' in lines[main_start_line+2]):
+            lines2.insert(main_start_line+1, '   import fire\n')
+            lines2.insert(main_start_line+2, '   fire.Fire()\n')
+
+    return lines2
+
+
+#################################################################################################################
 def get_file(file_path):
     """function _get_all_line
     Args:
@@ -167,245 +421,6 @@ def extrac_block(lines):
     return dd
 
 
-def normalize_header(file_name, lines):
-    """Nomarlize Header block
-
-    Args input is a array of lines.
-    """
-    #### not need of regex, code easier to read 
-
-    lines2 = []
-    if len(lines) >= 3:
-        # line 1
-        if '# -*- coding: utf-8 -*-' not  in lines[0] :
-            lines2.append('# -*- coding: utf-8 -*-\n')
-
-        # line 2
-        if 'MNAME' not in lines[1] :   ### MNAME = "utilmy.docs.format"
-            nmane =  ".".join( os.path.abspath(file_name).split("\\")[-4:] )[:-3]
-            lines2.append( f'MNAME = "{nmane}"\n')
-
-        if 'HELP' not in lines[2] :   ### HELP
-            lines2.append( f'HELP = "util"\n')
-
-    else:
-        lines2.append('# -*- coding: utf-8 -*-\n')
-        nmane =  ".".join( os.path.abspath(file_name).split("\\")[-4:] )[:-3]
-        lines2.append( f'MNAME = "{nmane}"\n')
-        lines2.append( f'HELP = "util"\n')
-
-    ### Add previous line
-    lines2.extend(lines)
-    # print(lines2)
-    return lines2
-
-
-def normalize_import(lines):
-    """  merge all import in one line and append others
-
-    """
-    import_list = []
-    from_list = []
-    lines2 = []
-    for line in lines :
-        if "import " in line :
-            if "from " in line : from_list.append(line)
-            else :               import_list.append(line)
-
-    ### Merge all import in one line   ################################################
-    llall = []
-    for imp in import_list :
-        imp = imp[6:] # remove import
-        ll    = [ t.strip() for t in  imp.split(",") if 'import' not in t ]
-        llall = llall + ll
-
-    lall = sorted( llall )
-
-    lines2 = []
-    ss = "import "
-    for mi in lall:
-        ss =  ss + mi + ", "
-        if len(ss) >  90 :
-            lines2.append( f"{ss[:-2]}\n" )  
-            ss = "import "
-    lines2.append(f"{ss[:-2]}\n")  
-
-    # lines2.extend(from_list)
-    # print(lines2)
-
-    #### Remaining import 
-    for ii, line in enumerate(lines):
-        if ii > 100 : break
-        if line.startswith("import ") : continue  ### Remove Old import
-        lines2.append(line)  
-
-    #### 
-    # print(lines2)
-    return lines2
-
-
-
-
-def normalize_logger(lines):
-    lines2 = []
-    if len(lines) >0:
-        if "from utilmy import log" not in lines[0]:
-            lines2.append('from utilmy import log, log2, help_create\n')
-    else:
-        lines2.append('from utilmy import log, log2, help_create\n')
-    # append all
-    lines2.extend(lines)
-
-    # check help function:
-    is_exist_help = False
-    for line in lines:
-        if line.startswith("def help"):
-            is_exist_help = True
-            break
-    
-    if not is_exist_help:
-        lines2.extend([
-            'def help():\n',
-            '   print( HELP + help_create(MNAME) )\n',
-            '\n',
-            '\n'
-        ])
-    # print(lines2)
-    return lines2
-
-
-def normalize_test(lines):
-    lines2 = []
-
-    # check test_all function:
-    is_exist_test_all = False
-    for line in lines:
-        if line.startswith("def test_all"):
-            is_exist_test_all = True
-            break
-    
-    if not is_exist_test_all:
-        lines2.extend([
-            'def test_all() -> None:\n',
-            '    """function test_all"""\n',
-            '    log(MNAME)\n',
-            '    test1()\n',
-            '    test2()\n',
-            '\n',
-            '\n',
-        ])
-
-    # check test1 function:
-    is_exist_test1 = False
-    for line in lines:
-        if line.startswith("def test1"):
-            is_exist_test1 = True
-            break
-    
-    if not is_exist_test1:
-        lines2.extend([
-            'def test1() -> None:\n',
-            '    """function test"""\n',
-            '    pass\n',
-            '\n',
-            '\n',
-        ])
-
-    # # check test2 function:
-    # is_exist_test2 = False
-    # for line in lines:
-    #     if line.startswith("def test2"):
-    #         is_exist_test2 = True
-    #         break
-    
-    # if not is_exist_test2:
-    #     lines2.extend([
-    #         'def test2() -> None:\n',
-    #         '    """function test"""\n',
-    #         '    pass\n',
-    #         '\n',
-    #         '\n',
-    #     ])
-
-    ###### Eztend lines
-    lines2.extend(lines)
-    return lines2
-
-def normalize_core(lines):
-    return lines
-
-
-def normalize_footer(lines):
-
-    lines2 = []
-
-    # check test_all function:
-    main_start_line = 0
-    is_exist_main = False
-    for ii, line in enumerate(lines):
-        if line.startswith("if __name__"):
-            is_exist_main = True
-            main_start_line = ii
-            break
-    if not is_exist_main:
-        lines2.extend(lines)
-        lines2.extend([
-            'if __name__ == "__main__":\n',
-            '   import fire\n',
-            '   fire.Fire()\n',
-        ])
-    else:
-        lines2.extend(lines)
-        if  not ('import fire' in lines[main_start_line+1]  and 'fire.Fire()' in lines[main_start_line+2]):
-            lines2.insert(main_start_line+1, '   import fire\n')
-            lines2.insert(main_start_line+2, '   fire.Fire()\n')
-
-    return lines2
-
-
-def read_and_normalize_file(file_path):
-    all_lines = get_file(file_path)
-    info = extrac_block(all_lines)
-
-    new_headers = normalize_header(file_path, info['header'])
-    new_imports = normalize_import(info['import'])
-    new_loggers = normalize_logger(info['logger'])
-    new_tests =   normalize_test(info['test'])
-    new_cores =   normalize_core(info['core'])
-    new_footers = normalize_footer(info['footer'])
-
-    # Create new data array then write to new file
-    new_all_lines = []
-    new_all_lines.extend(new_headers)
-    new_all_lines.extend(new_imports)
-    new_all_lines.extend(new_loggers)
-    new_all_lines.extend(new_tests)
-    new_all_lines.extend(new_cores)
-    new_all_lines.extend(new_footers)
-
-    # ss = "\n".join(new_all_lines)
-    return new_all_lines
-
-
-
-
-def read_and_normalize_file2(file_path, output_file):
-    new_all_lines = read_and_normalize_file(file_path)
-    # new_all_lines = ss.split("\n")
-    print(new_all_lines)
-    with open(output_file, 'w+', encoding='utf-8') as f:
-        f.writelines(new_all_lines)
-
-
-
-def read_and_normalize_file3(file_path, output_file):
-    ## Safe Modification
-    batch_format_file(in_file= file_path, dirout= output_file, 
-                    format_list= [ read_and_normalize_file ])
-
-
-
-
 #############################################################################################
 if 'check if .py compile':
     def batch_format_file(in_file:str, dirout:str, format_list:list):
@@ -484,13 +499,8 @@ if 'check if .py compile':
 
 
 if __name__ == '__main__':
-    read_and_normalize_file2('test_script/test_script_no_header.py', 'test_script/output/test_script_no_header.py')
-    read_and_normalize_file2('test_script/test_script_no_logger.py', 'test_script/output/test_script_no_logger.py')
-    read_and_normalize_file2('test_script/test_script_no_core.py', 'test_script/output/test_script_no_core.py')
-    read_and_normalize_file2('test_script/test_script_normalize_import.py', 'test_script/output/test_script_normalize_import.py')
+    import fire 
+    # fire.Fire()
+    test1()
 
 
-    read_and_normalize_file3('test_script/test_script_normalize_import.py', 'test_script/output')
-    read_and_normalize_file3('test_script/test_script_no_header.py', 'test_script/output')
-    read_and_normalize_file3('test_script/test_script_no_logger.py', 'test_script/output')
-    read_and_normalize_file3('test_script/test_script_no_core.py', 'test_script/output')
