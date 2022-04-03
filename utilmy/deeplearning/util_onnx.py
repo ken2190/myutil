7be4e3c68171bbf20273879ddde6d284d0ab72e6
anhdang000 <danglinh.anh000@gmail.com>
2022-04-03 16:10:28 +0700

    add test_onnx_convert

--- utilmy/deeplearning/util_onnx.py
+++ utilmy/deeplearning/util_onnx.py
@@ -420,12 +420,53 @@ def test_create_model_pytorch(dirsave=None, model_name=""):
         return SuperResolutionNet  ## return the class
 
 
+
+def test_onnx_convert():
+    import torch.nn as nn
+    import torch.nn.init as init
+
+    class SuperResolutionNet(nn.Module):
+        def __init__(self, upscale_factor, inplace=False):
+            super(SuperResolutionNet, self).__init__()
+
+            self.relu = nn.ReLU(inplace=inplace)
+            self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
+            self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
+            self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+            self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
+            self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+
+            self._initialize_weights()
+
+        def forward(self, x):
+            x = self.relu(self.conv1(x))
+            x = self.relu(self.conv2(x))
+            x = self.relu(self.conv3(x))
+            x = self.pixel_shuffle(self.conv4(x))
+            return x
+
+        def _initialize_weights(self):
+            init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
+            init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
+            init.orthogonal_(self.conv3.weight, init.calculate_gain('relu'))
+            init.orthogonal_(self.conv4.weight)
+
+    # Create the super-resolution model by using the above model definition.
+    torch_model = SuperResolutionNet(upscale_factor=3)
+
+    model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100-44c6958e.pth'
+
+    onnx_convert(torch_model, checkpoint_path=model_url)
+
+
+
 ########################################################################################################
 ############## Core Code ###############################################################################
 def onnx_convert(
-    model_path, 
-    dirout, 
-    input_shape,
+    torch_model, 
+    input_shape=(1, 224, 224),
+    dirout='.', 
+    checkpoint_path=None,
     export_params=True,
     onnx_version=10, 
     do_constant_folding=True, 
@@ -435,7 +476,8 @@ def onnx_convert(
     """Core function to convert a pytorch model to onnx
 
     Args:
-        model (str): model to be converted
+        torch_model: model object to load state dict
+        checkpoint_path (str): path to checkpoint file
         dirout (str): directory to save the onnx model
         input_shape (tuple): input shape to run model to export onnx model.
         onnx_version (int, optional): onnx version to convert the model. Defaults to 10.
@@ -444,17 +486,25 @@ def onnx_convert(
         output_names (list, optional): output names of the model. Defaults to ['output'].
         dynamic_axes (dict, optional): variable length axes. Defaults to {'input' : {0 : 'batch_size'}, 'output' : {0 : 'batch_size'}}.
     """
-    filename = '.'.join(os.path.basename(model_path).split('.')[:-1])
+    filename = '.'.join(os.path.basename(checkpoint_path).split('.')[:-1])
     out_path = os.path.join(dirout, filename + '.onnx')
     os.makedirs(dirout, exist_ok=True)
 
-    model = torch.load(model_path)
-    model.eval()
+    if checkpoint_path is not None:
+        if torch.cuda.is_available():
+            map_location = None
+        else:
+            map_location=torch.device('cpu')
+        torch_model.load_state_dict(model_zoo.load_url(checkpoint_path, map_location=map_location))
+    else:
+        torch_model.load_state_dict(checkpoint_path)
+
+    torch_model.eval()
 
     x = torch.rand(1, *input_shape, requires_grad=True)
-    out = model(x)
+    out = torch_model(x)
     torch.onnx.export(
-        model, 
+        torch_model, 
         x,
         out_path,
         export_params=export_params,
