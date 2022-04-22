2595a46527b695e22f11674567f58b452d1ddc88
arita37 <noelkev0@gmail.com>
2022-04-22 13:08:02 +0900

    ok

--- utilmy/zdocstring.py
+++ utilmy/zdocstring.py
@@ -1,27 +1,50 @@
 # -*- coding: utf-8 -*-
 """Example Google style docstrings.
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
-Example:
-    Examples can be given using either the ``Example`` or ``Examples``
-    sections. Sections support any reStructuredText formatting, including
-    literal blocks::
-        $ python example_google.py
-Section breaks are created by resuming unindented text. Section breaks
-are also implicitly created anytime a new section starts.
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
-Todo:
-    * For module TODOs
-    * You have to also use ``sphinx.ext.todo`` extension
-.. _Google Python Style Guide:
-   http://google.github.io/styleguide/pyguide.html
+
+    Args:
+        param1 (int): The first parameter.
+        param2 (:obj:`str`, optional): The second parameter. Defaults to None.
+            Second line of description should be indented.
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Returns:
+        bool: True if successful, False otherwise.
+        Following lines should be indented to match the first line.
+        The ``Returns`` section supports any reStructuredText formatting,
+        including literal blocks::
+
+            {   'param1': param1,
+                'param2': param2
+            }
+
+    Example:
+        Usage 1::
+
+          import numpy as np
+          a  = np.cos(1)
+
+
+        Actual Docstring code ::
+
+          This is an example of a module level function.
+          Args:
+              param1 (int): The first parameter.
+              param2 (:obj:`str`, optional): The second parameter. Defaults to None.
+                  Second line of description should be indented.
+              *args: Variable length argument list.
+              **kwargs: Arbitrary keyword arguments.
+
+          Returns:
+              bool: True if successful, False otherwise.
+
+
+          Example:
+              including literal blocks::
+
+                 ok = np.cos(5)
+
+
 """
 
 
