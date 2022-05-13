17a5d5d70e71b532abdb5f0ecc3417a6ad48ecc7
arita37 <noelkev0@gmail.com>
2022-05-13 20:57:44 +0900

    ok

--- utilmy/deeplearning/ttorch/model_ensemble.py
+++ utilmy/deeplearning/ttorch/model_ensemble.py
@@ -239,6 +239,154 @@ def test2_new():
 
 
 ##############################################################################################
+class BaseModel(object):
+    """This is BaseClass for model create
+
+    Method:
+        create_model : Initialize Model (torch.nn.Module)
+        evaluate: 
+        prepro_dataset:  (conver pandas.DataFrame to appropriate format)
+        create_loss :   Initialize Loss Function 
+        training:   starting training
+        build: create model, loss, optimizer (call before training)
+        train: equavilent to model.train() in pytorch (auto enable dropout,vv..vv..)
+        eval: equavilent to model.eval() in pytorch (auto disable dropout,vv..vv..)
+        device_setup: 
+        load_DataFrame: read pandas
+        load_weight: 
+        save_weight: 
+        predict : 
+    """
+    
+    def __init__(self,arg):
+        self.arg = Box(arg)
+        self._device = self.device_setup(arg)
+        self.losser = None
+        self.is_train = False
+        
+    @abstractmethod
+    def create_model(self,) -> torch.nn.Module:
+    #   raise NotImplementedError
+        log("       model is building")
+    @abstractmethod
+    def evaluate(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def prepro_dataset(self,csv) -> pd.DataFrame:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def create_loss(self,) -> torch.nn.Module:
+        log("       loss is building")
+        # raise NotImplementedError
+
+    @abstractmethod
+    def training(self,):
+        raise NotImplementedError
+
+    @property
+    def device(self,):
+        return self._device
+    
+    @device.setter
+    def device(self,value):
+        if isinstance(value,torch.device):
+          self._device = value
+        elif isinstance(value,str):
+          self._device = torch.device(value)
+        else:
+          raise TypeError("device must be str or torch.device")
+
+    def build(self,):
+        self.net = self.create_model().to(self.device)
+        self.loss_calc= self.create_loss().to(self.device)
+        # self.loss_calc= 
+        self.is_train = False
+    
+    def train(self): # equivalent model.train() in pytorch
+        self.is_train = True
+        self.net.train()
+
+    def eval(self):     # equivalent model.eval() in pytorch
+        self.is_train = False
+        self.net.eval()
+
+    def device_setup(self,arg):
+        device = getattr(arg,'device','cpu')
+        seed   = arg.seed if hasattr(arg,'seed') else 42
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+        if 'gpu' in device :
+            try :
+                torch.cuda.manual_seed(seed)
+                torch.cuda.manual_seed_all(seed)
+                torch.backends.cudnn.deterministic = True
+                torch.backends.cudnn.benchmark = False
+            except Exception as e:
+                log(e)
+                device = 'cpu'
+        return device
+
+    def load_DataFrame(self,path=None)-> pd.DataFrame:
+        if path:
+            log(f"reading csv from {path}")
+            self.df = pd.read_csv(path,delimiter=';')
+            return self.df
+        if os.path.isfile(self.arg.dataset.path):
+            log(f"reading csv from arg.DATASET.PATH :{self.arg.dataset.path}")
+            self.df = pd.read_csv(self.arg.dataset.path,delimiter=';')
+            return self.df
+        else:
+            import requests
+            import io
+            r = requests.get(self.arg.dataset.url)
+            log(f"Reading csv from arg.DATASET.URL")
+            if r.status_code ==200:
+                self.df = pd.read_csv(io.BytesIO(r.content),delimiter=';')
+            else:
+                raise Exception("Can't read data, status_code: {r.status_code}")
+            
+            return self.df
+
+
+    def load_weights(self, path):
+        assert os.path.isfile(path),f"{path} does not exist"
+        try:
+          ckp = torch.load(path,map_location=self.device)
+        except Exception as e:
+          log(e)
+          log(f"can't load the checkpoint from {path}")  
+        if isinstance(ckp,collections.OrderedDict):
+          self.net.load_state_dict(ckp)
+        else:
+          self.net.load_state_dict(ckp['state_dict'])
+    
+    def save_weight(self,path,meta_data=None):
+      os.makedirs(os.path.dirname(path),exist_ok=True)
+      ckp = {
+          'state_dict':self.net.state_dict(),
+      }
+      if meta_data:
+        if isinstance(meta_data,dict):
+            ckp.update(meta_data)
+        else:
+            ckp.update({'meta_data':meta_data,})
+            
+        
+      torch.save(ckp,path)
+
+    def predict(self,x,**kwargs):
+        # raise NotImplementedError
+        output = self.net(x,**kwargs)
+        return output 
+
+
+
+
+##############################################################################################
 class MergeModel_create(BaseModel):
     """
     """
@@ -527,152 +675,6 @@ class modelA_create(BaseModel):
         super(modelA_create,self).create_loss()
         return torch.nn.BCELoss()
 
-
-
-
-class BaseModel(object):
-    """This is BaseClass for model create
-
-    Method:
-        create_model : Initialize Model (torch.nn.Module)
-        evaluate: 
-        prepro_dataset:  (conver pandas.DataFrame to appropriate format)
-        create_loss :   Initialize Loss Function 
-        training:   starting training
-        build: create model, loss, optimizer (call before training)
-        train: equavilent to model.train() in pytorch (auto enable dropout,vv..vv..)
-        eval: equavilent to model.eval() in pytorch (auto disable dropout,vv..vv..)
-        device_setup: 
-        load_DataFrame: read pandas
-        load_weight: 
-        save_weight: 
-        predict : 
-    """
-    
-    def __init__(self,arg):
-        self.arg = Box(arg)
-        self._device = self.device_setup(arg)
-        self.losser = None
-        self.is_train = False
-        
-    @abstractmethod
-    def create_model(self,) -> torch.nn.Module:
-    #   raise NotImplementedError
-        log("       model is building")
-    @abstractmethod
-    def evaluate(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def prepro_dataset(self,csv) -> pd.DataFrame:
-        raise NotImplementedError
-    
-    @abstractmethod
-    def create_loss(self,) -> torch.nn.Module:
-        log("       loss is building")
-        # raise NotImplementedError
-
-    @abstractmethod
-    def training(self,):
-        raise NotImplementedError
-
-    @property
-    def device(self,):
-        return self._device
-    
-    @device.setter
-    def device(self,value):
-        if isinstance(value,torch.device):
-          self._device = value
-        elif isinstance(value,str):
-          self._device = torch.device(value)
-        else:
-          raise TypeError("device must be str or torch.device")
-
-    def build(self,):
-        self.net = self.create_model().to(self.device)
-        self.loss_calc= self.create_loss().to(self.device)
-        # self.loss_calc= 
-        self.is_train = False
-    
-    def train(self): # equivalent model.train() in pytorch
-        self.is_train = True
-        self.net.train()
-
-    def eval(self):     # equivalent model.eval() in pytorch
-        self.is_train = False
-        self.net.eval()
-
-    def device_setup(self,arg):
-        device = getattr(arg,'device','cpu')
-        seed   = arg.seed if hasattr(arg,'seed') else 42
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
-
-        if 'gpu' in device :
-            try :
-                torch.cuda.manual_seed(seed)
-                torch.cuda.manual_seed_all(seed)
-                torch.backends.cudnn.deterministic = True
-                torch.backends.cudnn.benchmark = False
-            except Exception as e:
-                log(e)
-                device = 'cpu'
-        return device
-
-    def load_DataFrame(self,path=None)-> pd.DataFrame:
-        if path:
-            log(f"reading csv from {path}")
-            self.df = pd.read_csv(path,delimiter=';')
-            return self.df
-        if os.path.isfile(self.arg.dataset.path):
-            log(f"reading csv from arg.DATASET.PATH :{self.arg.dataset.path}")
-            self.df = pd.read_csv(self.arg.dataset.path,delimiter=';')
-            return self.df
-        else:
-            import requests
-            import io
-            r = requests.get(self.arg.dataset.url)
-            log(f"Reading csv from arg.DATASET.URL")
-            if r.status_code ==200:
-                self.df = pd.read_csv(io.BytesIO(r.content),delimiter=';')
-            else:
-                raise Exception("Can't read data, status_code: {r.status_code}")
-            
-            return self.df
-
-
-    def load_weights(self, path):
-        assert os.path.isfile(path),f"{path} does not exist"
-        try:
-          ckp = torch.load(path,map_location=self.device)
-        except Exception as e:
-          log(e)
-          log(f"can't load the checkpoint from {path}")  
-        if isinstance(ckp,collections.OrderedDict):
-          self.net.load_state_dict(ckp)
-        else:
-          self.net.load_state_dict(ckp['state_dict'])
-    
-    def save_weight(self,path,meta_data=None):
-      os.makedirs(os.path.dirname(path),exist_ok=True)
-      ckp = {
-          'state_dict':self.net.state_dict(),
-      }
-      if meta_data:
-        if isinstance(meta_data,dict):
-            ckp.update(meta_data)
-        else:
-            ckp.update({'meta_data':meta_data,})
-            
-        
-      torch.save(ckp,path)
-
-    def predict(self,x,**kwargs):
-        # raise NotImplementedError
-        output = self.net(x,**kwargs)
-        return output 
 
 
 
