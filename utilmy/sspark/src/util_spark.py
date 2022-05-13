70013c2cd540eeda82c4fa4eafbc1f88d0334599
fang-sh <1282888595@qq.com>
2022-05-14 00:18:24 +0800

    modify spark_config_check

--- utilmy/sspark/src/util_spark.py
+++ utilmy/sspark/src/util_spark.py
@@ -132,17 +132,11 @@ def spark_config_check():
 
     for env_path in env_vars_required:
         path = os.environ.get(env_path)
-        if path:
-            log("exists: " + env_path + " = " + path)
-        else:
-            log("not exists: " + env_path)
+        log("exists: " + env_path + " = " + path) if path else log("not exists: " + env_path)
 
     for file in file_required:
         file_path = os.path.expandvars(file)
-        if os.path.exists(file_path):
-            log("exist: " + file_path)
-        else:
-            log("not exists: " + file_path)    
+        log("exist: " + file_path) if os.path.exists(file_path) else log("not exists: " + file_path) 
 
 
 def spark_config_create(mode='', dirout):
