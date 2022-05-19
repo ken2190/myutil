27f7f82706062a286e2386a382fb0384fc59373b
Kev NoNo <arita37@users.noreply.github.com>
2022-05-19 15:13:51 +0900

    ok

--- utilmy/sspark/src/util_spark.py
+++ utilmy/sspark/src/util_spark.py
@@ -430,7 +430,7 @@ def spark_df_timeseries_split(df_m:sp_dataframe, splitRatio:float, sparksession:
     return df_train, df_test
 
 
-def spark_df_table_dedupe(df,partition_by='month',order_by='dt'):
+def spark_df_filter_mostrecent(df,partition_by='month',order_by='dt'):
     """ get Lastest col
     """
     dedupe_df = df.withColumn('rnk__',F.row_number().over(Window.partitionBy(partition_by).orderBy(F.desc(order_by))))\
