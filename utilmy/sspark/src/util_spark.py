a97f0e114b30c04b22169a92405f8ab7a97fd20e
Kev NoNo <arita37@users.noreply.github.com>
2022-06-02 15:26:52 +0900

    ok

--- utilmy/sspark/src/util_spark.py
+++ utilmy/sspark/src/util_spark.py
@@ -180,6 +180,12 @@ def test2():
     dfres  = spark_df_filter_mostrecent(df=df, colid='id', col_orderby='residency_date', decreasing=1, rank=1)
     log(dfres.show())
 
+    dfsamp = spark_df_sampleunder(df=df,coltarget = "city", major_label="LA",minor_label = "LI",ratio=0.1)
+    log(dfsamp.show())
+
+    dftest = spark_df_isempty(df)
+    log(dftest)
+
 
 
 
